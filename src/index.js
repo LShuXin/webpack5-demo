a22d78b57d3e08630f994f8e117d80e055918f80
liushuxin <liushuxin@comeon.com>
2022-12-27 18:41:30 +0800

    使用HtmlWebpackPlugin 注入所有entry

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
- function component() {
-   const element = document.createElement('div');
+import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   // lodash 在当前 script 中使用 import 引入
-   element.innerHTML = _.join(['Hello', '哈哈哈'], ' ');
-   element.classList.add('hello');
+  //  element.innerHTML = _.join(['Hello', '哈哈哈'], ' ');
+  //  element.classList.add('hello');
 
    // 将图像添加到我们已经存在的 div 中。
    const myIcon = new Image();
